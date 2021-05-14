62828e1b237807f142be55806612b35fdc46ceec
David <davidstankoviic@gmail.com>
2021-05-14 19:09:56 +0200

    Initialize: App, Firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef } from 'react';
+
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  //config
+})
+
+
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/signin.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import firebase from 'firebase/app';
+// const auth = firebase.auth();
+
+// export default function SignIn() {
+//     const signInWithGoogle = () => {
+//         const provider = new firebase.auth.GoogleAuthProvider();
+//         auth.signInWithPopup(provider);
+//     }
+
+//     return (
+//         <button onClick={signInWithGoogle}>Sign in with Google</button>
+//     )
+// }
